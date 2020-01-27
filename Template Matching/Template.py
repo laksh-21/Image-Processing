eeeda877f39a202c628c977f17863f9a04841211
laksh-21 <54995644+laksh-21@users.noreply.github.com>
2020-01-28 03:56:10 +0530

    removed ececutables

--- Template Matching/Template.py
+++ Template Matching/Template.py
@@ -1,3 +1,7 @@
 import easygui
 import cv2
 
+from os.path import abspath, dirname
+BASE_PATH = abspath(dirname(__file__))
+FONT_PATH = BASE_PATH + '/fonts/'
+print(FONT_PATH)
--- Thresholding/Threshold.py
+++ Thresholding/Threshold.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy
+from os.path import abspath, dirname
 
-img = cv2.imread('F:/Codes/PythonCodes/Image Processing/OpenCV/Thresholding/data/data.jpg', 0)
+BASE_PATH = abspath(dirname(__file__))
+DATA_PATH = BASE_PATH + '/data/'
+IMG_PATH = DATA_PATH + 'data.jpg'
+img = cv2.imread(IMG_PATH, 0)
 
 ret, thres = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
 gauss = cv2.adaptiveThreshold(img, 200, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 2)
